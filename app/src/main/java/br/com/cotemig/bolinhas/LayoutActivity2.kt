6f71abc10e27f99301360772f189e12b2890d652
Rafhaela Marques <57140533+rafhaelamarques@users.noreply.github.com>
2022-03-08 19:58:49 -0300

    Add files via upload

---
+++ app/src/main/java/br/com/cotemig/bolinhas/LayoutActivity2.kt
@@ -0,0 +1,11 @@
+package br.com.cotemig.bolinhas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout2)
+    }
+}
---
+++ app/src/main/java/br/com/cotemig/bolinhas/LayoutActivity9.kt
@@ -0,0 +1,11 @@
+package br.com.cotemig.bolinhas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LayoutActivity9 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout9)
+    }
+}
